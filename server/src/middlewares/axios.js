70d02fbf141d2f58b5839a71c92f9271ef88ddd8
coldnanee <pmihaylov036@gmail.com>
2024-04-17 13:05:03 +0300

    add redis for requests

--- server/src/middlewares/axios.js
+++ server/src/middlewares/axios.js
@@ -1,19 +1,33 @@
 import axios, { AxiosError } from "axios";
 
-export const axiosMiddleware = (req, res, next) => {
+import { Config } from "../config/config.js";
+
+import { redisClient } from "../redis/client.js";
+
+export const axiosMiddleware = async (req, res, next) => {
 	if (req.method !== "GET") {
 		return res.status(400).json({ message: 'Can use only "GET" method' });
+	}
+
+	const cacheData = await redisClient.get(req.originalUrl);
+
+	if (cacheData) {
+		const parsedData = JSON.parse(cacheData);
+		return res.json({ ...parsedData });
 	}
 
 	return axios
 		.get(`https://api.kinopoisk.dev/v1.4${req.originalUrl}`, {
 			headers: {
-				"X-API-KEY": process.env.API_KEY
+				"X-API-KEY": Config.get("API_KEY")
 			}
 		})
-		.then(({ status, data }) => {
-			return res.status(status).json({ ...data });
-		})
+		.then(({ status, data }) =>
+			redisClient
+				.setEx(req.originalUrl, 600, JSON.stringify(data))
+				.then(() => res.status(status).json({ ...data }))
+				.catch((e) => res.status(500).json({ message: "Can't setup cache" }))
+		)
 		.catch((e) => {
 			if (e instanceof AxiosError) {
 				const err = e.response.data;
